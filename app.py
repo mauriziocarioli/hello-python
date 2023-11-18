c41886c61d65a58853fb2523b7063e7d2fefb1c6
Maurizio Carioli <maurizio.carioli@gmail.com>
2023-11-18 11:29:43 -0500

    Update app.py to add flask web site

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from datetime import datetime
+#from datetime import datetime
 
 # Get the current date and time
-current_datetime = datetime.now()
+#current_datetime = datetime.now()
 
-print("Current date and time: ", current_datetime)
+#print("Current date and time: ", current_datetime)
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
 
+@app.route('/')
+def show_current_time():
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return f'Current date and time: {current_time}'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
